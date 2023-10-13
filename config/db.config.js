5d036aad16e2049e55d61d281198c4d77708ad90
SrimayeeBoyina <107919144+SrimayeeBoyina@users.noreply.github.com>
2023-10-13 12:40:58 +0530

    Delete config directory

--- config/db.config.js
+++
@@ -1,7 +0,0 @@
-const mysql = require('mysql2');
-
-const dbOptions = require('./dbOptions.config');
-
-const connectionPool = mysql.createPool(dbOptions);
-
-module.exports = connectionPool;
--- config/dbOptions.config.js
+++
@@ -1,9 +0,0 @@
-const dbOptions = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'atm_system',
-  port: '3306'
-}
-
-module.exports = dbOptions;
